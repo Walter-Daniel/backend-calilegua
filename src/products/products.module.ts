4366aab9d5abd0627b82876e3e71056d67fa4400
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2024-10-29 23:53:33 -0300

    update:  product module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -5,19 +5,20 @@ import { ProductsController } from './controllers/products.controller';
 import { CategoriesService } from './services/categories.service';
 import { ManufacturersService } from './services/manufacturers.service';
 import { ProductsService } from './services/products.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [
     CategoriesController,
     ManufacturersController,
-    ProductsController
+    ProductsController,
   ],
   providers: [
     CategoriesService,
-    ManufacturersService,
-    ProductsService
-  ],
-  exports:[ProductsService]
+    ManufacturersService, 
+    ProductsService],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
